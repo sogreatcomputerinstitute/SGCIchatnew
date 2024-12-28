a64ee82793955b5be64640f407adaf72ba7f05bd
sogreatcomputerinstitute <destiny20202010@gmail.com>
2024-12-28 02:10:10 -0800

    Add files via upload

---
+++ chatroom/public/anim.js
@@ -0,0 +1,11 @@
+function openimg(){
+    document.getElementById("upload").style.width = "100%";
+    document.getElementById("upload").style.height = "100%";
+    document.getElementById("close").style.fontSize = "20px";
+    document.getElementById("close").style.fontStyle = "bold";
+}
+function closeimg(){
+    document.getElementById("upload").style.width = "0";
+    document.getElementById("upload").style.height = "0";
+    document.getElementById("close").style.fontSize = "0";
+}
